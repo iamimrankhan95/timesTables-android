f404be2c39c50f79fa2eb75f693c2b5774d6421a
IMRAN AHMED KHAN <IMRAN AHMED KHAN@DESKTOP-8MISFBN>
2019-03-15 17:13:09 +0600

    set to min value 1 of seekbar

--- app/src/main/java/com/example/timestables/MainActivity.java
+++ app/src/main/java/com/example/timestables/MainActivity.java
@@ -13,6 +13,8 @@ import static android.widget.SeekBar.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    SeekBar timesTablesSeekBar;
+    ListView timesTableListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,26 +22,30 @@ public class MainActivity extends AppCompatActivity {
         timesTables();
     }
     protected void timesTables(){
-        SeekBar timesTablesSeekBar = findViewById(R.id.seekBar);
+        timesTablesSeekBar = findViewById(R.id.seekBar);
+        timesTableListView= findViewById(R.id.timesTables);
         timesTablesSeekBar.setMax(20);
-        final ListView timesTableView= findViewById(R.id.timesTables);
-        final ArrayList<Integer> timesTable=new ArrayList<Integer>();
-
+        timesTablesSeekBar.setProgress(1);
+        ArrayList<Integer> timesTable=new ArrayList<Integer>();
         for(int a=1;a<=10;a++){
             timesTable.add(a*1);
         }
         ArrayAdapter<Integer> readyTimesTable=new ArrayAdapter<Integer>(getBaseContext(), android.R.layout.simple_list_item_1,timesTable);
-        timesTableView.setAdapter(readyTimesTable);
+        timesTableListView.setAdapter(readyTimesTable);
         timesTablesSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                final ArrayList<Integer> timesTable2=new ArrayList<Integer>();
+                if(progress<1){
+                    timesTablesSeekBar.setProgress(1);//Any variable defined in a method and accessed by an anonymous inner class must be final. Otherwise, you could use that variable in the inner class, unaware that if the variable changes in the inner class, and then it is used later in the enclosing scope, the changes made in the inner class did not persist in the enclosing scope. Basically, what happens in the inner class stays in the inner class.
+                    progress=1;
+                }
+                ArrayList<Integer> timesTable2=new ArrayList<Integer>();
 
                 for(int a=1;a<=10;a++){
                     timesTable2.add(a*progress);
                 }
                 ArrayAdapter<Integer> readyTimesTable=new ArrayAdapter<Integer>(getApplicationContext(),android.R.layout.simple_list_item_1,timesTable2);
-                timesTableView.setAdapter(readyTimesTable);
+                timesTableListView.setAdapter(readyTimesTable);
             }
 
             @Override
